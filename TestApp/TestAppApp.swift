67d90989b551972f0a98b40bfc86b7517d918420
Han, Jusung <jhan80@illinois.edu>
2025-05-28 21:24:02 -0700

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Jusung Han on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
